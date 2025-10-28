d46f4527ab393b62daf15d1a43bd4f534cdf1caf
Harshu1006 <bhardwajharsh2244@gmail.com>
2025-10-28 22:20:12 +0530

    Update Vowel and consonant.py

--- python competative/Strings/Vowel and consonant.py
+++ python competative/Strings/Vowel and consonant.py
@@ -5,10 +5,11 @@ for _ in range(T):
     vowel_count = 0
     consonant_count = 0
     for i in s:
-        if i isalpha():
-                if i in "aeiouAEIOU":
+        if i.isalpha():
+            if i in "aeiouAEIOU":
             vowel_count+=1
-        else:
+            else:
             consonant_count+=1
 
     print(f"vowels: {vowel_count} consonants: {consonant_count}")
+
